462a163722451d97a4f1b1731e96ee30d0f47cff
CRISTIAN VAGNER MAYDANA JURADO <cristian.vmjurado@senacsp.edu.br>
2022-05-13 11:54:05 -0300

    Finalização de cadastro

--- terceiro/src/App.js
+++ terceiro/src/App.js
@@ -39,7 +39,7 @@ return (
   </div>
 )*/
 // ----------------------------------------------
-
+/* 
 import { useState } from "react";
 function App() {
   const [n, setN] = useState (1);
@@ -53,3 +53,67 @@ function App() {
 }
 
 export default App;
+*/
+// -------------------------------------------
+
+import { useState } from "react";
+function App() {
+  const [produto, setProduto] = useState ([
+    {
+      id:500,
+      nome:"Mouse",
+      categoria:"Informatica",
+      quantidade:15,
+      preco:50.00 
+    },
+
+    {
+      id:501,
+      nome:"Teclado",
+      categoria:"Informatica",
+      quantidade:20,
+      preco:150.00 
+    },
+  ]);
+
+  return (
+    <div>
+      
+      <button onClick={() => {
+        
+       var cod = prompt("Entre com o código");
+       var no = prompt("Entre com o nome do produto");
+       var cat = prompt("Entre com a categoria");
+       var qt = prompt("Entre com a quantidade");
+       var  pr = prompt ("Entre com o preço");
+        
+        setProduto(produto.concat({
+          id:cod,
+          nome:no,
+          categoria:cat,
+          quantidade:qt,
+          preco:pr 
+      }))}}
+
+      ></button>
+      
+      
+      {produto.map((item,index) => (
+
+        <div key={index}> 
+        <h2>{item.nome}</h2>
+        <ul>
+          <li>Código: {item.id}</li>
+          <li>Categoria: {item.categoria}</li>
+          <li>Quantidade: {item.quantidade}</li>
+          <li>Preço: {item.preco}</li>
+        </ul>
+      </div>
+      ))}
+
+    </div>
+  );
+} 
+
+export default App;
+
